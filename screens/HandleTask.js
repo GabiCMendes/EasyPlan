e83db80097f9b816f85a3b2b6c4e2f2c6a6e49d8
GabiCMendes <gabrielacarvalhomendes2008@gmail.com>
2024-01-12 17:59:23 -0300

    easy plan

--- screens/HandleTask.js
+++ screens/HandleTask.js
@@ -1,80 +1,88 @@
 import React from "react";
-import {View, Text, StyleSheet, TouchableOpacity} from "react-native"
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native"
+const starImg = require("../assets/star.png")
 
 export default class HandleTask extends React.Component {
     constructor() {
         super();
         this.state = {
-          score: 0,
-          isPressed: false
+            score: 100,
+            isPressed: false
         };
-      }
-      render() {
-        return (
-          <View style={styles.container}>
-            <View style={styles.headerContainer}>
-              <Text style={styles.headerText}>Vamos Começar</Text>
-              <Text style={styles.headerScore}>{this.state.score}</Text>
-              <Text style={styles.icon}>*</Text>
-            </View>
-            <View style={styles.taskContainer}>
-                <TouchableOpacity style={styles.checkTask}>
-                    <Text> </Text>
-                </TouchableOpacity>
-                <Text style={styles.nameTask}>Tomar Água</Text>
-                <Text style={styles.pointsTask}>5+</Text>
-                <Text style={styles.icon}>*</Text>
-            </View>
-          </View>
-        );
-      }
     }
-    
-    const styles = StyleSheet.create({
-        container:{
-            flex:1,
-            backgroundColor:"#fff"
-        },
-        headerContainer:{
-            flex:0.2,
-            justifyContent:"space-evenly",
-            alignItems:"center",
-            flexDirection:"row"
-        },
-        headerText:{
-            color: "#004A70",
-            fontWeight: "900",
-            fontSize: 40
-        },
-        headerScore:{
-            fontSize: 30
-        },
-        icon:{
-            fontSize: 60,
-            color: "gold",
-        },
-        taskContainer:{
-            flex:0.8,
-            justifyContent:"space-evenly",
-            alignItems:"center",
-            flexDirection:"row",
-            borderColor:"black",
-            borderWidth:1
-        },
-        checkTask:{
-            backgroundColor:"#00A5AB",
-            width:50,
-            height:50,
-            borderRadius:100,
-            borderWidth:2,
-            borderStyle: "double"
-            
-        },
-        nameTask:{
-            backgroundColor:"#00A5AB"
-        },
-        pointsTask:{
-            
-        }
-    
-    });
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.headerContainer}>
+                    <Text style={styles.headerText}>Vamos Começar</Text>
+                    <View style ={styles.scoreContainer}>
+                        <Text style={styles.headerScore}>{this.state.score}</Text>
+                        <Image style={styles.icon} source={starImg} />
+                    </View>
+                </View>
+                <View style={styles.taskContainer}>
+                    <TouchableOpacity style={styles.checkTask}>
+                        <Text> </Text>
+                    </TouchableOpacity>
+                    <Text style={styles.nameTask}>Tomar Água</Text>
+                    <Text style={styles.pointsTask}>5+</Text>
+                    <Image style={styles.icon} source={starImg} />
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff"
+    },
+    headerContainer: {
+        flex: 0.2,
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        flexDirection: "row"
+    },
+    headerText: {
+        color: "#004A70",
+        fontWeight: "900",
+        fontSize: 40
+    },
+   scoreContainer: {
+    justifyContent: "center",
+        alignItems: "center",
+   },
+     headerScore: {
+        fontSize: 30
+    },
+    icon: {
+        width: 50,
+        height: 70,
+        // position: "absolute"
+    },
+    taskContainer: {
+        flex: 0.8,
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        flexDirection: "row",
+        borderColor: "black",
+        borderWidth: 1
+    },
+    checkTask: {
+        backgroundColor: "#00A5AB",
+        width: 50,
+        height: 50,
+        borderRadius: 100,
+        borderWidth: 2,
+        // borderStyle: "double"
+
+    },
+    nameTask: {
+        backgroundColor: "#00A5AB"
+    },
+    pointsTask: {
+
+    }
+
+});
